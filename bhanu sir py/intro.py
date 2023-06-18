1e02885f28a1ffeee655fd7c27345e6ce810cc20
Pragati00713 <kushwahapragati713@gmail.com>
2023-06-18 14:26:27 +0530

    html project sample

---
+++ bhanu sir py/intro.py
@@ -0,0 +1,3 @@
+# introdution-Guido van Rossum(2008)(pyhton3)
+print("pragati")
+print("pragati *5")
---
+++ bhanu sir py/string.py
@@ -0,0 +1,5 @@
+first_name="pragati "
+last_name="kushwaha "
+full_name=first_name+last_name
+print(full_name *5) 
+
